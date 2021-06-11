2d56f983eab2a1856e34426dc90792ebc1cf6f11
PopovMihail1 <mihailpopov728@gmail.com>
2021-06-11 07:47:09 +0300

    Add files via upload

---
+++ strategy/src/com/company/Operation.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public abstract class Operation {
+    public abstract void sort(int mass[]);
+}
---
+++ strategy/src/com/company/Sorting.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Sorting {
+
+    private Operation operation = null;
+
+    public Sorting(int operation){
+
+        switch(operation) {
+
+            case 1: this.operation = new SortByMax();
+
+                break;
+
+            case 2: this.operation = new SortByMin();
+
+                break;
+
+            default: System.out.println(
+
+                    "Такая операция отсутствует"); }
+
+    }
+
+    public void sorting(int[] mass) {
+
+        if (operation != null) operation.sort(mass);
+
+        else return;
+
+    }
+
+}
